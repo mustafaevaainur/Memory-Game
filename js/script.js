a4679ff8a4b353c0eec3f7eb69ff194309002793
Ainur <79215797496@yandex.ru>
2018-02-04 00:44:56 +0300

    fill array

---
+++ js/script.js
@@ -0,0 +1,317 @@
+var cards = [
+    {
+        name: "0C",
+        img: "https://image.ibb.co/deca4R/0C.png",
+        id: 1
+    },
+
+    {
+        name: "0D",
+        img: "https://image.ibb.co/jK4NjR/0D.png",
+        id: 2
+    },
+
+    {
+        name: "0H",
+        img: "https://image.ibb.co/nJ56B6/0H.png",
+        id: 3
+    },
+
+    {
+        name: "0S",
+        img: "https://image.ibb.co/gXihjR/0S.png",
+        id: 4
+    },
+
+    {
+        name: "2C",
+        img: "https://image.ibb.co/dwR2jR/2C.png",
+        id: 5
+    },
+
+    {
+        name: "2D",
+        img: "https://image.ibb.co/b1Ttr6/2D.png",
+        id: 6
+    },
+
+    {
+        name: "2H",
+        img: "https://image.ibb.co/dDPYr6/2H.png",
+        id: 7
+    },
+
+    {
+        name: "2S",
+        img: "https://image.ibb.co/bBiRB6/2S.png",
+        id: 8
+    },
+
+    {
+        name: "3C",
+        img: "https://image.ibb.co/hc7mB6/3C.png",
+        id: 9
+    },
+
+    {
+        name: "3D",
+        img: "https://image.ibb.co/nmXa4R/3D.png",
+        id: 10
+    },
+
+    {
+        name: "3H",
+        img: "https://image.ibb.co/jMXWdm/3H.png",
+        id: 11
+    },
+
+    {
+        name: "3S",
+        img: "https://image.ibb.co/hKrPym/3S.png",
+        id: 12
+    },
+
+    {
+        name: "4C",
+        img: "https://image.ibb.co/erJhjR/4C.png",
+        id: 13
+    },
+
+    {
+        name: "4D",
+        img: "https://image.ibb.co/mSKoPR/4D.png",
+        id: 14
+    },
+
+    {
+        name: "4H",
+        img: "https://image.ibb.co/kona4R/4H.png",
+        id: 15
+    },
+
+    {
+        name: "4S",
+        img: "https://image.ibb.co/hbeoPR/4S.png",
+        id: 16
+    },
+
+    {
+        name: "5C",
+        img: "https://image.ibb.co/c6MPym/5C.png",
+        id: 17
+
+    },
+
+    {
+        name: "5D",
+        img: "https://image.ibb.co/jxr2jR/5D.png",
+        id: 18
+    },
+
+    {
+        name: "5H",
+        img: "https://image.ibb.co/mYnWdm/5H.png",
+        id: 19
+    },
+
+    {
+        name: "5S",
+        img: "https://image.ibb.co/iTOhjR/5S.png",
+        id: 20
+    },
+
+    {
+        name: "6C",
+        img: "https://image.ibb.co/gAGeW6/6C.png,",
+        id: 21
+    },
+
+    {
+        name: "6D",
+        img: "https://image.ibb.co/dN34ym/6D.png",
+        id: 22
+    },
+
+    {
+        name: "6H",
+        img: "https://image.ibb.co/kTzcJm/6H.png",
+        id: 23
+    },
+
+    {
+        name: "6S",
+        img: "https://image.ibb.co/hcG2jR/6S.png",
+        id: 24
+    },
+
+    {
+        name: "7C",
+        img: "https://image.ibb.co/h2xWdm/7C.png",
+        id: 25
+    },
+
+    {
+        name: "7D",
+        img: "https://image.ibb.co/jrqv4R/7D.png",
+        id: 26
+    },
+
+    {
+        name: "7H",
+        img: "https://image.ibb.co/gmQ6B6/7H.png",
+        id: 27
+    },
+
+    {
+        name: "7S",
+        img: "https://image.ibb.co/gnBPym/7S.png",
+        id: 28
+    },
+
+    {
+        name: "8C",
+        img: "https://image.ibb.co/mC28PR/8C.png",
+        id: 29
+    },
+
+    {
+        name: "8D",
+        img: "https://image.ibb.co/j9M2jR/8D.png",
+        id: 30
+    },
+
+    {
+        name: "8H",
+        img: "https://image.ibb.co/hEOtr6/8H.png",
+        id: 31
+    },
+
+    {
+        name: "8S",
+        img: "https://image.ibb.co/iHttr6/8S.png",
+        id: 32
+    },
+
+    {
+        name: "9C",
+        img: "https://image.ibb.co/hGvTPR/9C.png",
+        id: 33
+    },
+
+    {
+        name: "9D",
+        img: "https://image.ibb.co/jCRDr6/9D.png",
+        id: 34
+    },
+
+    {
+        name: "9H",
+        img: "https://image.ibb.co/hvAZym/9H.png",
+        id: 35
+    },
+
+    {
+        name: "9S",
+        img: "https://image.ibb.co/dp23r6/9S.png",
+        id: 36
+    },
+
+    {
+        name: "AC",
+        img: "https://image.ibb.co/dMfCjR/AC.png",
+        id: 37
+    },
+
+    {
+        name: "AD",
+        img: "https://image.ibb.co/fQuuym/AD.png",
+        id: 38
+    },
+
+    {
+        name: "AH",
+        img: "https://image.ibb.co/iPvdPR/AH.png",
+        id: 39
+    },
+
+    {
+        name: "AS",
+        img: "https://image.ibb.co/cLU54R/AS.png",
+        id: 40
+    },
+
+    {
+        name: "JC",
+        img: "https://image.ibb.co/gUwUW6/JC.png",
+        id: 41
+    },
+
+    {
+        name: "JD",
+        img: "https://image.ibb.co/hWRUW6/JD.png",
+        id: 42
+    },
+
+    {
+        name: "JH",
+        img: "https://image.ibb.co/hXDnJm/JH.png",
+        id: 43
+    },
+
+    {
+        name: "JS",
+        img: "https://image.ibb.co/nogUW6/JS.png",
+        id: 44
+    },
+
+    {
+        name: "KC",
+        img: "https://image.ibb.co/dgcQ4R/KC.png",
+        id: 45
+    },
+
+
+
+    {
+        name: "KD",
+        img: "https://image.ibb.co/jc6JPR/KD.png",
+        id: 46
+    },
+
+    {
+        name: "KH",
+        img: "https://image.ibb.co/fJ2sjR/KH.png",
+        id: 47
+    },
+
+    {
+        name: "KS",
+        img: "https://image.ibb.co/fowJPR/KS.png",
+        id: 48
+    },
+
+    {
+        name: "QC",
+        img: "https://image.ibb.co/iBsEym/QC.png",
+        id: 49
+    },
+
+    {
+        name: "QD",
+        img: "https://image.ibb.co/mchEym/QD.png",
+        id: 50
+    },
+
+    {
+        name: "QH",
+        img: "https://image.ibb.co/cmv1dm/QH.png",
+        id: 51
+    },
+
+    {
+        name: "QS",
+        img: "https://image.ibb.co/iq7sjR/QS.png",
+        id: 52
+    }
+];
+
